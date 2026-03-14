c30977fcba04c8ff0c8370fce566c12642a1d67a
Donald Reilly Jr <donald-reilly@users.noreply.github.com>
2026-03-13 20:58:42 -0700

    Add: Initial layout

--- src/constructor/constructor.py
+++ src/constructor/constructor.py
@@ -1,3 +1,5 @@
+from persistance import BPersistent
+from figman import FigMan
 class BConstructed():
     """
     BConstructed takes custom json serialized tkinter objects and reconstructs them during runtime.
@@ -6,5 +8,18 @@ class BConstructed():
         pass
     def __call__(self):
         pass
-    def _construct_window(self):
+    def _get_window_config(self):
         pass
+
+    def _construct_window(self, window):
+        window_attributes_map = {
+            "title": window.title,
+            "geometry": window.geometry,
+            "minsize": window.minsize,
+            "maxsize": window.maxsize,
+            "resizable": window.resizable,
+            "iconbitmap": window.iconbitmap,
+            "state": window.state,
+            "attributes": window.attributes,
+        } 
+    
