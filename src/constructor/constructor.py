6215da65d77e5644df4b79fe74e4542868015633
Donald Reilly Jr <donald-reilly@users.noreply.github.com>
2026-03-13 18:09:55 -0700

    Initial commit

---
+++ src/constructor/constructor.py
@@ -0,0 +1,10 @@
+class BConstructed():
+    """
+    BConstructed takes custom json serialized tkinter objects and reconstructs them during runtime.
+    """
+    def __init__(self):
+        pass
+    def __call__(self):
+        pass
+    def _construct_window(self):
+        pass
