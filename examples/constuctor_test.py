1fad233eabf81772b44526a6c2bc2dc1c5505a95
Donald Reilly Jr <donald-reilly@users.noreply.github.com>
2026-03-17 23:41:06 -0700

    add: constructor test

--- examples/constuctor_test.py
+++ examples/constuctor_test.py
@@ -1,5 +1,14 @@
 from constructor import BConstructed
+from serializer import BSerialized
 
-new_constructor = BConstructed()
-new_root = new_constructor("examples/tkinter_window_test.json")
+builder = BConstructed()
+serializer = BSerialized()
+
+
+def capture_state(root):
+    new_config = serializer()
+    serializer.save_configuration("examples/capture_state_test.json", new_config)
+
+    
+new_root = builder("examples/tkinter_window_test.json")
 new_root["app"].mainloop()
